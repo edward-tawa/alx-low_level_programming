edbd39c8fd6faf4bc0ae947d2166cc55c9b11361
edward tawa <tagumatawa@.com>
2023-03-17 14:15:58 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,9 +8,22 @@
  */
 int main(void)
 {
-    srand(time(0));
-    int n = rand();
-    int mod = n%10;
-    printf("%d\n",n);
-    printf("%d\n",mod);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
 }
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+return (0);
+}
+
