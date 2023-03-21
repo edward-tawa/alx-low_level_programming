e5585c09f6eee5dac4f8e2728e30e9a8f1ec1e04
edward tawa <tagumatawa@.com>
2023-03-21 09:31:57 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * print alphabet - printing alphabet ten times
+ * Return void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; a <= 'z' c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
