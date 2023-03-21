0e17b211d976fafac5a73febe51ecec1b8bba922
edward tawa <tagumatawa@.com>
2023-03-21 14:09:41 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character belongs to the alphabet
+ * @c: the character to be checked
+ * Return: 1 for alphabetic character or 2 for anything else
+ */
+
+int _isalpha(int c)
+
+{
+
+	if ((c >= 65 && c <= 98)) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+	
+}
