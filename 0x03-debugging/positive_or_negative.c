2bd1683434a2d1687d0560af11811f7eeb575627
edward tawa <tagumatawa@.com>
2023-03-23 09:21:21 -0700

    positive_or_negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+
+	else if (i < 0)
+		print("%d is negative\n", i);
+
+	else
+		printf("%d is zero\n", i);
+}
