aa85de062cdf45fe01b055f85494b2dd02a89b0e
edward tawa <tagumatawa@.com>
2023-04-04 15:55:21 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - function to locate character
+ * @s: input string
+ * @c: character
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (0);
+}
+
