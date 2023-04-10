e4c43ed545569df2df6c22cb6823668f09658368
edward tawa <tagumatawa@.com>
2023-04-10 14:44:19 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints name of program
+ * @argc:  argument counter
+ * @argv: arguments array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
+
