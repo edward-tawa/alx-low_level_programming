48580e0ca416b0b9ced83e99d68e775cd08e9af7
edward tawa <tagumatawa@.com>
2023-04-17 19:19:03 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * struct dog - structure of dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: onewr of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+};
