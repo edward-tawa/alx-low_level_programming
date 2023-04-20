be45039a944a0fd00ce0737636d4c04bf7864e37
edward tawa <tagumatawa@.com>
2023-04-20 13:57:06 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,14 +11,14 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum = 0;
 	unsigned int i;
-	var_list ptr;
+	va_list ptr;
 
-	var_start(ptr, n);
+	va_start(ptr, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += var_arg(ptr, unsigned int);
+		sum += va_arg(ptr, unsigned int);
 	}
-	var_end(ptr);
+	va_end(ptr);
 	return (sum);
 }
