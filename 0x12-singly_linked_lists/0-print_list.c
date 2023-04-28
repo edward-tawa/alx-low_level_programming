5d468382451f0cf01a903fd2787b50d721982726
edward tawa <tagumatawa@.com>
2023-04-28 13:24:57 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints the elements of a linked list
+ * @h: pointer to the starting node
+ *
+ * Return: number of nodes in the linked list
+ */
+size_t print_list(const list_t *h)
+{
+	int s = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
+
