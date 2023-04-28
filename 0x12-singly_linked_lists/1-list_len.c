85a64588faa350db876065fc658a2245d6851a9d
edward tawa <tagumatawa@.com>
2023-04-28 13:37:45 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - function that counts and returns the number of elements in a linked list
+ * @h: pointer to the starting node
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
+
