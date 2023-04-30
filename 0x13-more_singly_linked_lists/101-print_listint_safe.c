e8d5e6473372f05bcacfc19f5041ebfe6991a842
edward tawa <tagumatawa@.com>
2023-04-30 13:44:18 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include<stdlib.h>
+#include <stdio.h>
+
+/**
+ * looped_listint_len - function that counts the number of unique nodes
+ * @head: starting node of listint_t
+ * Return: If the list is not looped - 0.
+ * Otherwise - number of unique nodes
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				nodes++;
+				slow = slow->next;
+				fast = (fast->next)->next;
+			}
+
+			slow = slow->next;
+			while (slow != fast)
+			{
+				nodes++;
+				slow = slow->next;
+			}
+
+			return (nodes);
+		}
+
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (0);
+}
+
