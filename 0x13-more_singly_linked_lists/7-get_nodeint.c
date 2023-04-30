3883ac2d0369e7ad107cb24e239daec685807e0e
edward tawa <tagumatawa@.com>
2023-04-29 22:24:48 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,11 +16,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (i = 1; i < index - 1; i++)
+	for (i = 0; i < index - 1; i++)
 	{
 		temp = temp->next;
 	}
 
-	return (temp);
+	if (temp)
+		return (temp);
+	return (NULL);
 }
 
