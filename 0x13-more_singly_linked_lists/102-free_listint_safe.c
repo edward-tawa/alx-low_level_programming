ca49c5aa4748c7829520592efcb5f32544e858c5
edward tawa <tagumatawa@.com>
2023-05-01 14:27:35 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -12,10 +12,10 @@ size_t free_listint_safe(listint_t **h)
 	listint_t *temp;
 	size_t length = 0;
 
-	if (h == NULL || *h == NULL)
+	if (!h || !*h)
 		return (0);
 
-	while (*h == NULL)
+	while (*h != NULL)
 	{
 		addr_diff = *h - (*h)->next;
 		if (addr_diff > 0)
