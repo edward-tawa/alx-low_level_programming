f9b873e67b72f7f98e2502301f3b80825d9b2f7a
Edward <tagumatawa@gmail.com>
2023-08-18 13:55:58 -0700

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
+ * free_dlistint - frees a linked list
  * @head: listint_t list to be freed
  */
 void free_dlistint(dlistint_t *head)
