d1f65209bc6b0b99d33f4c4a1e618dba1fc19861
Edward <tagumatawa@gmail.com>
2023-08-18 12:52:06 -0700

    0-print_dlistint.c

--- 0-print_dlistint.c
+++ 0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
+ * print_dlistint - prints all the elements of a linked list
  * @h: linked list of type dlistint_t to print
  * Return: number of nodes
  */
