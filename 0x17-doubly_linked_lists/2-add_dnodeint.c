518bed153aadc6f080a78403f5a696e8dc038c20
Edward <tagumatawa@gmail.com>
2023-08-18 13:49:49 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,7 +6,7 @@
  * @n: data to insert in that new node
  * Return: pointer to the new node, or NULL if it fails
  */
-dlistint_t *add_nodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -6,7 +6,7 @@
  * @n: data to insert in the new element
  * Return: pointer to the new node, or NULL if it fails
  */
-dlistint_t *add_nodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 	dlistint_t *temp = *head;
